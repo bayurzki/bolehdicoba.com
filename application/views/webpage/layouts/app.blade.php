b213134eaa83e1748e942f9f08830bf759147950
bayurzki <bayu@bolehdicoba.com>
2022-04-14 11:32:33 +0700

    style.1.1

--- application/views/webpage/layouts/app.blade.php
+++ application/views/webpage/layouts/app.blade.php
@@ -44,7 +44,7 @@
         @yield('title')
     </title>    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/webpage/css/style.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/webpage/css/style.css?v=1.1">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/webpage/css/owl.carousel.min.css">
     <style type="text/css">
 
